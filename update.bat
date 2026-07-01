0dd4138e0510665966d44ca7cf19d81b30b4cdc6
sSasha <copjing@gmail.com>
2026-07-01 17:25:47 +1000

    update

--- update.bat
+++ update.bat
@@ -4,4 +4,4 @@ git add .
 git commit -m "update"
 git push origin main
 
-ssh RPi02W@192.168.20.15 "echo "RPi02W" | sudo -S -v && sudo systemctl restart discordbots && sudo journalctl -u discordbots -f"
+ssh RPi02W@192.168.20.15 "echo "RPi02W" | sudo -S -v && echo "RPi02W" | sudo -S systemctl restart discordbots && echo "RPi02W" | sudo -S journalctl -u discordbots -f"
