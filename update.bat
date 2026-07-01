be30c55e39b0162f3a9053e4a2217a33c3f74848
sSasha <copjing@gmail.com>
2026-07-01 17:16:14 +1000

    update

--- update.bat
+++ update.bat
@@ -4,4 +4,4 @@ git add .
 git commit -m "update"
 git push origin main
 
-ssh rpi02n "SUDO_ASKPASS=~/sudo.sh && sudo -A -v && sudo systemctl restart discord-bots && journalctl -u discord-bots -f"
+ssh RPi02W@192.168.20.15 "SUDO_ASKPASS=~/sudo.sh && sudo -A -v && sudo systemctl restart discord-bots && journalctl -u discord-bots -f"
